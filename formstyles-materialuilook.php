7a62775e614a192671c696bfd4a48e85b257a07c
digital6s <50987853+digital6s@users.noreply.github.com>
2020-09-03 16:38:45 +1000

    Create formstyles-materialuilook.php
    Setting up files - boilerplate

---
+++ formstyles-materialuilook.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Digital 6s Material Design Look for Forms Lite
+ * Plugin URI: https://github.com/digital6s/Googlemap-wp
+ * Description: A very lite lite form styling plugin to add material design looking styles to forms, There is no admin section on purpose. You can change the look and function of styles in the css and js file, Instructions and resources to replace and customise the code is in the js file 
+ * Version: 1.0
+ * Author:            Digital6s - Vanessa
+ * Author URI:        https://digital6s.com
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * 
+ **/
